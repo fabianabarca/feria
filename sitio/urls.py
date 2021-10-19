a664a96e982f22e7eea75d6e3a7f145b33d6a877
Tyron Fonseca Alvarado <tyron.fonseca@ucr.ac.cr>
2021-10-18 18:45:59 -0600

    Fixed problem with media url

--- sitio/urls.py
+++ sitio/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('inicio.urls')),
     path('ferias/', include('ferias.urls')),
     path('admin/', admin.site.urls)    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
