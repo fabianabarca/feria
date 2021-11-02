9f78b58ae9b0302c555b5a4888784c6bfb9d72d3
Tyron Fonseca Alvarado <tyron.fonseca@ucr.ac.cr>
2021-11-01 18:42:10 -0600

    Merge development into tyrons playgroung

--- sitio/settings.py
+++ sitio/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'ferias',
     'inicio',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- sitio/urls.py
+++ sitio/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 urlpatterns = [
     path('', include('inicio.urls')),
     path('ferias/', include('ferias.urls')),
-    path('admin/', admin.site.urls)    
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
