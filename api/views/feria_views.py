779c5e53834c5aedfbb9eda0508e9c7155600b93
Tyron Fonseca <tyron.fonseca@hotmail.com>
2021-11-24 11:34:33 -0600

    Filtro basado en geolocalización
    -Ahora las ferias se pueden filtrar dada una ubicación (latitud, longitud) y un radio
    -Se agrega método para verificar distancia entre puntos geograficos

--- api/views/feria_views.py
+++ api/views/feria_views.py
@@ -2,14 +2,14 @@
 # Clases encargadas del API relacionado a Feria
 #
 # Author: Tyron Fonseca - tyron.fonseca@ucr.ac.cr
-# Last modified: 20/11/2021
+# Last modified: 24/11/2021
 # ===================================================
 
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from ferias.models import Feria
 from api.serializers.feria_serializer import FeriaSerializer
-from api.views.utils import DynamicFieldsViewMixin
+from api.views.utils import DynamicFieldsViewMixin, FeriasHelper
 
 
 class FeriaList(DynamicFieldsViewMixin, generics.ListAPIView):
@@ -23,6 +23,30 @@ class FeriaList(DynamicFieldsViewMixin, generics.ListAPIView):
     search_fields = ['nombre', 'provincia',
                      'canton', 'distrito', 'horarios__dia_inicio',
                      'horarios__dia_final']
+
+    def get_queryset(self):
+        ferias_id_filtered = []
+        ferias = Feria.objects.all()
+        # Parametros del URL
+        lat = self.request.query_params.get('lat')
+        lon = self.request.query_params.get('lon')
+        radius = self.request.query_params.get('radius')
+        # Filtrar por latitud, longitud y radio
+        if lat is not None and lon is not None and radius is not None:
+            index = 0
+            for feria in ferias.iterator():
+                # Verificar si esta en el radio
+                if FeriasHelper.is_in_radius(float(lat), float(lon),
+                                             feria.latitud, feria.longitud,
+                                             int(radius)):
+                    # Agregar ID de la feria
+                    ferias_id_filtered.insert(index, feria.ferias_id)
+                    index = index + 1
+            # Filtrar las ferias que esten es el radio dado
+            return ferias.filter(ferias_id__in=ferias_id_filtered)
+        else:
+            # Devolver todas las ferias si falta alguno de los parametros
+            return ferias
 
 
 class FeriaDetail(DynamicFieldsViewMixin, generics.RetrieveAPIView):
--- api/views/utils.py
+++ api/views/utils.py
@@ -2,8 +2,33 @@
 # Clases encargadas que ayudan a las vistas
 #
 # Author: Tyron Fonseca - tyron.fonseca@ucr.ac.cr
-# Last modified: 20/11/2021
+# Last modified: 24/11/2021
 # ===================================================
+
+import math
+
+
+class FeriasHelper:
+    def is_in_radius(lat1, lon1, lat2, lon2, radius):
+        """ Calcular distancia de dos coordenadas usando la formula Haversine
+            https://www.movable-type.co.uk/scripts/latlong.html
+        """
+
+        radius_earth = 6371e3  # Radio de la tierra en metros
+        pi_radian = (math.pi/180)
+        phi_1 = lat1 * pi_radian
+        phi_2 = lat2 * pi_radian
+        delta_phi = (lat2 - lat1) * pi_radian
+        delta_lambda = (lon2 - lon1) * pi_radian
+
+        a = math.sin(delta_phi/2)**2 + math.cos(phi_1) * \
+            math.cos(phi_2) * math.sin(delta_lambda/2) ** 2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+        distance = radius_earth * c
+        if distance <= radius:
+            return True
+        else:
+            return False
 
 
 class DynamicFieldsViewMixin(object):
